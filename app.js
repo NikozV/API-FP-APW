3f8b0a625c78a9f69aeb013020a09407be5c6fff
Nico <vassallo.mn@gmail.com>
2023-03-10 19:38:35 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const qrcode = require('qrcode');
+const { Client, MessageMedia } = require('whatsapp-web.js');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+// Configurar body-parser para analizar JSON y urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configurar multer para procesar la carga de archivos
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
+
+const client = new Client({ puppeteer: { args: ['--no-sandbox'] } });
+
+let qrResponse;
+
+client.on('qr', async (qr) => {
+  const qrImage = await qrcode.toDataURL(qr, { scale: 8 });
+  const html = `
+    <html>
+      <body>
+        <div style="text-align: center;">
+          <img src="${qrImage}" alt="WhatsApp QR Code">
+          <p>Scanear el QR para entrar - facilpos.co</p>
+        </div>
+      </body>
+    </html>
+  `;
+  if (qrResponse) {
+    qrResponse.send(html);
+  }
+});
+
+client.on('ready', () => {
+  console.log('Conetado!');
+});
+
+client.initialize();
+
+function sendErrorResponse(res, message) {
+  res.status(400).json({ response: { error: message } });
+}
+
+const sendMessageToNumber = async (number, message, res) => {
+  const chat = await client.getChatById(`${number}@c.us`);
+  if (!chat) {
+    sendErrorResponse(res, `No se pudo encontrar el chat con el número ${number}`);
+    return;
+  }
+  await chat.sendMessage(message);
+};
+
+const sendImageToNumber = async (number, imagePath, caption, res) => {
+  const chat = await client.getChatById(`${number}@c.us`);
+  if (!chat) {
+    sendErrorResponse(res, `No se pudo encontrar el chat con el número ${number}`);
+    return;
+  }
+
+  const imageBuffer = fs.readFileSync(imagePath);
+  const imageBase64 = imageBuffer.toString('base64');
+  const imageMedia = new MessageMedia('image/jpeg', imageBase64);
+
+  await chat.sendMessage(imageMedia, { caption: caption });
+};
+
+// Ruta para enviar mensajes
+app.post('/api/send-message', async (req, res) => {
+  const { number, message } = req.body;
+  await sendMessageToNumber(number, message, res);
+  res.send({ response: 'Mensaje enviado con éxito' });
+});
+// Ruta para enviar imágenes
+app.post('/api/send-image', upload.single('image'), async (req, res) => {
+  const { number, message } = req.body;
+  const imagePath = req.file.path;
+  await sendImageToNumber(number, imagePath, message, res);
+  res.send({ response: 'Imagen enviada con éxito' });
+});
+app.get('/', (req, res) => {
+  qrResponse = res;
+});
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado en el puerto 3000');
+});
+
